9127f10036b7a46d66de541ef6edf53f03a55a4c
pallavibhatt <pallavibhatt1593@gmail.com>
2020-05-16 20:11:32 +0530

    Applied DOM changes using style properties in javascript

---
+++ exercise-2/js/app.js
@@ -0,0 +1,16 @@
+function closeMe(){
+	//find the element
+	x=document.getElementById('demo');
+	//Option 1: Change the style attribute directly
+	x.style.display="none";
+
+}
+
+function openMe(){
+
+//find the element
+	x=document.getElementById('demo');
+	//Option 1: Change the style attribute directly
+	x.style.display="block";
+
+}
